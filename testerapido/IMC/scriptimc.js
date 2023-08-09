5c81418e1d16f120c1d196eb37cdfeaeac3ac493
Kk4yn4 <133814829+Kk4yn4@users.noreply.github.com>
2023-08-09 20:52:17 -0300

    calculo de imc

---
+++ testerapido/IMC/scriptimc.js
@@ -0,0 +1,12 @@
+//peso / altura*altura
+/*let pessoa = {
+    nome:'kaynã',
+    sobrenome:'Silva',
+    idade:21,
+    peso:110,
+    altura:1.90,
+    
+}
+let imc = pessoa.peso / (pessoa.altura*pessoa.altura)
+
+console.log(`${pessoa.nome} ${pessoa.sobrenome} tem ${pessoa.idade} anos, pesa ${pessoa.peso} kg tem ${pessoa.altura} de altura e seu IMC é de ${imc}`)*/
