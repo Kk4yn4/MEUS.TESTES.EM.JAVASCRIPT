8abf09d1134bc8d7d29edc070e16f7ee50540e10
Kk4yn4 <133814829+Kk4yn4@users.noreply.github.com>
2023-08-17 23:31:11 -0300

    teste de imc finalizado

--- testerapido/IMC/scriptimc.js
+++ testerapido/IMC/scriptimc.js
@@ -1,12 +1,40 @@
-//peso / altura*altura
-/*let pessoa = {
-    nome:'kaynã',
-    sobrenome:'Silva',
-    idade:21,
-    peso:110,
-    altura:1.90,
-    
-}
-let imc = pessoa.peso / (pessoa.altura*pessoa.altura)
 
-console.log(`${pessoa.nome} ${pessoa.sobrenome} tem ${pessoa.idade} anos, pesa ${pessoa.peso} kg tem ${pessoa.altura} de altura e seu IMC é de ${imc}`)*/
+
+ function finalizar(){
+    let nome = document.getElementById('nometxt')
+    let sobren = document.getElementById('sobrentxt')
+    let idade = document.getElementById('idadetxt')
+    let peso = document.getElementById('pesotxt')
+    let altura = document.getElementById('alturatxt')
+    let res = document.getElementById('res')
+    let rest = document.querySelector('div#ress')
+    let idad = Number(idade.value)
+    let pes = Number(peso.value)
+    let alt = Number(altura.value)
+ 
+
+
+    let n = nome.value;
+    let sn = sobren.value 
+    if(n == nome.value|| sn == sobren.value){
+        
+        res.innerHTML = (`Olá ${n} ${sn} sejá bem vindo!`)
+    }
+    if(n == ''|| sn == ''){
+        res.innerHTML=('[ERROR] TEM INFORMAÇÕES FALTANDO!')
+    }
+    
+   
+    if(idade >=1 || pes>=1 || alt>=1){
+       rest.innerHTML=(`${idad} ${pes} ${alt}`)
+        
+     }
+   
+    /*if(idad <= 0 || pes <= 0 || alt <=0){
+        alert('erro')
+    }*/
+
+    
+
+
+ }
